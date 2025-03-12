2f0717ae47c70671a37e950072118858b8a6d747
Pavel Ashifin <globus40000@gmail.com>
2025-03-12 12:38:27 +0600

    refactor: use destructuring

--- src/main.tsx
+++ src/main.tsx
@@ -46,18 +46,18 @@ if (!root) {
 createRoot(root).render(
   <StrictMode>
     <h1>Restaurants</h1>
-    {restaurants.map((restaurant) => (
-      <div key={restaurant.id}>
-        <h2>{restaurant.name}</h2>
+    {restaurants.map(({ id, name, menu, reviews }) => (
+      <div key={id}>
+        <h2>{name}</h2>
         <h3>Menu</h3>
         <ul>
-          {restaurant.menu.map((dish) => (
+          {menu.map((dish) => (
             <li key={dish.id}>{dish.name}</li>
           ))}
         </ul>
         <h3>Reviews</h3>
         <ul>
-          {restaurant.reviews.map((review) => (
+          {reviews.map((review) => (
             <li key={review.id}>{review.text}</li>
           ))}
         </ul>
