76e0dfd93019379dc3fcbe02da6c2918933a3166
Pavel Ashifin <globus40000@gmail.com>
2025-03-13 20:52:20 +0600

    feat: add Restaurants component with tabs

--- src/components/App/app.tsx
+++ src/components/App/app.tsx
@@ -1,15 +1,13 @@
 import { restaurants } from "../../mocks/restaurants";
 import { Layout } from "../Layout/layout";
-import { Restaurant } from "../Restaurant/restaurant";
+import { Restaurants } from "../Restaurants/restaurants";
 
 export const App = () => {
   return (
     <div className="app">
       <Layout>
         <h1>Restaurants</h1>
-        {restaurants.map((restaurant) => (
-          <Restaurant restaurant={restaurant} key={restaurant.id} />
-        ))}
+        <Restaurants restaurants={restaurants} />
       </Layout>
     </div>
   );
