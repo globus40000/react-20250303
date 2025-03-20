62423d7fab4f4a58516c48b3acc1264c9ab3328e
Pavel Ashifin <globus40000@gmail.com>
2025-03-20 19:49:09 +0600

    refactor(use-count): memoize callbacks

--- src/components/DishCounter/use-count.ts
+++ src/components/DishCounter/use-count.ts
@@ -1,18 +1,23 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export const useCount = (initial = 0, min?: number, max?: number) => {
   const [count, setCount] = useState(initial);
 
-  const increment = () => {
-    if (typeof max !== "number" || count < max) {
-      setCount(count + 1);
-    }
-  };
-  const decrement = () => {
-    if (typeof min !== "number" || count > min) {
-      setCount(count - 1);
-    }
-  };
+  const increment = useCallback(() => {
+    setCount((c) => {
+      const canIncrement = typeof max !== "number" || c < max;
+
+      return canIncrement ? c + 1 : c;
+    });
+  }, [max]);
+
+  const decrement = useCallback(() => {
+    setCount((c) => {
+      const canDecrement = typeof min !== "number" || c > min;
+
+      return canDecrement ? c - 1 : c;
+    });
+  }, [min]);
 
   return { count, increment, decrement };
 };
