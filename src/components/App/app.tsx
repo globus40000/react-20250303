b6cd7d021e32903f7b0ca0b7ca634c2f8923554a
Pavel Ashifin <globus40000@gmail.com>
2025-03-24 20:50:12 +0600

    style: reset css, add font

--- src/components/App/app.tsx
+++ src/components/App/app.tsx
@@ -2,6 +2,8 @@ import { FC } from "react";
 import { Layout } from "../Layout/layout";
 import { Restaurants } from "../Restaurants/restaurants";
 
+import "./app.css";
+
 export const App: FC = () => {
   return (
     <div className="app">
