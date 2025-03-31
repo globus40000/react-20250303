463f5a2c5ec85d29c689a2ce89f4a875b8cdd763
Pavel Ashifin <globus40000@gmail.com>
2025-04-01 01:08:01 +0600

    feat: add Header, Footer components

--- src/components/Layout/layout.tsx
+++ src/components/Layout/layout.tsx
@@ -1,13 +1,19 @@
 import { FC, PropsWithChildren } from "react";
 import { ProgressBar } from "../ProgressBar/progress-bar";
+import { Header } from "../Header/header";
+import { Footer } from "../Footer/footer";
 
 export const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className="layout">
       <ProgressBar />
-      <header>Header</header>
+      <header>
+        <Header />
+      </header>
       <section>{children}</section>
-      <footer>Footer</footer>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 };
