450257d26a6299cf56f87cbf5f56fa90fb22348a
Pavel Ashifin <globus40000@gmail.com>
2025-04-04 19:35:24 +0600

    feat: configure redux store

--- src/components/App/app.tsx
+++ src/components/App/app.tsx
@@ -3,18 +3,22 @@ import { Layout } from "../Layout/layout";
 import { RestaurantsPage } from "../RestaurantsPage/restaurants-page";
 import { ThemeContextProvider } from "../ThemeContextProvider/theme-context-provider";
 import { AuthContextProvider } from "../AuthContextProvider/auth-context-provider";
+import { Provider } from "react-redux";
+import { store } from "../../redux/store";
 
 import "./reset.css";
 import "./app.css";
 
 export const App: FC = () => {
   return (
-    <AuthContextProvider>
-      <ThemeContextProvider>
-        <Layout>
-          <RestaurantsPage />
-        </Layout>
-      </ThemeContextProvider>
-    </AuthContextProvider>
+    <Provider store={store}>
+      <AuthContextProvider>
+        <ThemeContextProvider>
+          <Layout>
+            <RestaurantsPage />
+          </Layout>
+        </ThemeContextProvider>
+      </AuthContextProvider>
+    </Provider>
   );
 };
