5c43980aa3b7bbcfce11febaa1a2a1213efae738
Pavel Ashifin <globus40000@gmail.com>
2025-04-10 19:45:03 +0600

    feat: get restaurants from api

--- src/pages/RestaurantsPage/restaurants-page-container.tsx
+++ src/pages/RestaurantsPage/restaurants-page-container.tsx
@@ -2,9 +2,18 @@ import { FC } from "react";
 import { useSelector } from "react-redux";
 import { selectRestaurantsIds } from "../../redux/entities/restaurant/slice";
 import { RestaurantsPage } from "./restaurants-page";
+import { getRestaurants } from "../../redux/entities/restaurant/get-restaurants";
+import { useRequest } from "../../redux/hooks/use-request";
 
 export const RestaurantsPageContainer: FC = () => {
+  // @ts-expect-error: Type 'unknown' is not assignable to type 'undefined'.
+  const requestStatus = useRequest(getRestaurants);
   const restaurantsIds = useSelector(selectRestaurantsIds);
 
-  return <RestaurantsPage restaurantsIds={restaurantsIds} />;
+  return (
+    <RestaurantsPage
+      restaurantsIds={restaurantsIds}
+      requestStatus={requestStatus}
+    />
+  );
 };
--- src/pages/RestaurantsPage/restaurants-page.tsx
+++ src/pages/RestaurantsPage/restaurants-page.tsx
@@ -1,24 +1,31 @@
 import { FC } from "react";
 import { RestaurantTabContainer } from "../../components/RestaurantTab/restaurant-tab-container";
-import { Identifier } from "../../types";
+import { Identifier, RequestStatus } from "../../types";
 import { Outlet } from "react-router";
+import { Skeleton } from "../../components/Skeleton/skeleton";
 
 import styles from "./restaurants-page.module.css";
 
 interface IRestaurantsPageProps {
   restaurantsIds: Identifier[];
+  requestStatus: RequestStatus;
 }
 
 export const RestaurantsPage: FC<IRestaurantsPageProps> = ({
   restaurantsIds,
+  requestStatus,
 }) => {
   return (
     <div className={styles.root}>
       <h1>Restaurants</h1>
       <div className={styles.tabs}>
-        {restaurantsIds.map((id) => (
-          <RestaurantTabContainer key={id} id={id} />
-        ))}
+        {requestStatus === RequestStatus.pending ? (
+          <Skeleton variant="rectangular" width={400} height={35} />
+        ) : (
+          restaurantsIds.map((id) => (
+            <RestaurantTabContainer key={id} id={id} />
+          ))
+        )}
       </div>
       <Outlet />
     </div>
