326090229d28341292164f1f08a699d4bf276e92
Pavel Ashifin <globus40000@gmail.com>
2025-04-17 15:52:52 +0600

    feat(redux utils): add getCommonRequestStatus function

--- src/redux/utils.ts
+++ src/redux/utils.ts
@@ -1,4 +1,4 @@
-import { Identifier } from "../types";
+import { Identifier, RequestStatus } from "../types";
 
 interface IEntity {
   id: Identifier;
@@ -14,4 +14,34 @@ export function getEntities<Item extends IEntity>(arr: Item[]) {
 
 export function getIds<Item extends IEntity>(arr: Item[]) {
   return arr.map<Item["id"]>(({ id }) => id);
+}
+
+export function getCommonRequestStatus(arr: RequestStatus[]) {
+  const stat = arr.reduce(
+    (acc, requestStatus) => {
+      acc[requestStatus] += 1;
+
+      return acc;
+    },
+    {
+      [RequestStatus.idle]: 0,
+      [RequestStatus.pending]: 0,
+      [RequestStatus.rejected]: 0,
+      [RequestStatus.fulfilled]: 0,
+    }
+  );
+
+  if (stat[RequestStatus.pending] > 0) {
+    return RequestStatus.pending;
+  }
+
+  if (stat[RequestStatus.rejected] > 0) {
+    return RequestStatus.rejected;
+  }
+
+  if (stat[RequestStatus.fulfilled] > 0) {
+    return RequestStatus.fulfilled;
+  }
+
+  return RequestStatus.idle;
 }
