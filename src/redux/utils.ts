33f9c0a464f7ced64303cf56c8b71223dcc71e37
Pavel Ashifin <globus40000@gmail.com>
2025-04-18 00:51:37 +0600

    refactor(redux utils): delete obsolete functions

--- src/redux/utils.ts
+++ src/redux/utils.ts
@@ -1,20 +1,4 @@
-import { Identifier, RequestStatus } from "../types";
-
-interface IEntity {
-  id: Identifier;
-}
-
-export function getEntities<Item extends IEntity>(arr: Item[]) {
-  return arr.reduce<Record<Identifier, Item>>((acc, item) => {
-    acc[item.id] = item;
-
-    return acc;
-  }, {});
-}
-
-export function getIds<Item extends IEntity>(arr: Item[]) {
-  return arr.map<Item["id"]>(({ id }) => id);
-}
+import { RequestStatus } from "../types";
 
 export function getCommonRequestStatus(arr: RequestStatus[]) {
   const stat = arr.reduce(
