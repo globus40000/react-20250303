f492b3253da78cb1f2820d26393ea0232592e921
Pavel Ashifin <globus40000@gmail.com>
2025-04-17 16:02:57 +0600

    feat: get users from api

--- src/components/Reviews/reviews-container.tsx
+++ src/components/Reviews/reviews-container.tsx
@@ -5,9 +5,12 @@ import { useRequest } from "../../redux/hooks/use-request";
 import { getReviewsForRestaurant } from "../../redux/entities/review/get-reviews-for-restaurant";
 import { useSelector } from "react-redux";
 import {
-  selectErrorMessage,
+  selectErrorMessage as selectReviewsErrorMessage,
   selectReviewsIds,
 } from "../../redux/entities/review/slice";
+import { getUsers } from "../../redux/entities/user/get-users";
+import { getCommonRequestStatus } from "../../redux/utils";
+import { selectErrorMessage as selectUsersErrorMessage } from "../../redux/entities/user/slice";
 
 interface IReviewsContainerProps {
   id: Identifier;
@@ -15,16 +18,22 @@ interface IReviewsContainerProps {
 
 export const ReviewsContainer: FC<IReviewsContainerProps> = ({ id }) => {
   // @ts-expect-error: Type 'unknown' is not assignable to type 'string'.
-  const requestStatus = useRequest(getReviewsForRestaurant, id);
+  const reviewsRequestStatus = useRequest(getReviewsForRestaurant, id);
+  // @ts-expect-error: Type 'unknown' is not assignable to type 'undefined'.
+  const usersRequestStatus = useRequest(getUsers);
   const reviewsIds = useSelector(selectReviewsIds);
-  const errorMessage = useSelector(selectErrorMessage);
+  const reviewsErrorMessage = useSelector(selectReviewsErrorMessage);
+  const usersErrorMessage = useSelector(selectUsersErrorMessage);
 
   return (
     <Reviews
       reviewsIds={reviewsIds}
       textNoReviews="No reviews yet."
-      requestStatus={requestStatus}
-      errorMessage={errorMessage}
+      requestStatus={getCommonRequestStatus([
+        reviewsRequestStatus,
+        usersRequestStatus,
+      ])}
+      errorMessage={reviewsErrorMessage || usersErrorMessage}
     />
   );
 };
