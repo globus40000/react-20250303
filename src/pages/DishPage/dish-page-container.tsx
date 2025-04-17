9baca6c673afbaf0544dd9495f8347ba7e9f20a0
Pavel Ashifin <globus40000@gmail.com>
2025-04-17 23:24:24 +0600

    feat: get dish by id from api

--- src/pages/DishPage/dish-page-container.tsx
+++ src/pages/DishPage/dish-page-container.tsx
@@ -2,19 +2,29 @@ import { FC } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router";
 import { IRootState } from "../../redux/store";
-import { selectDishById } from "../../redux/entities/dish/slice";
+import {
+  selectDishById,
+  selectErrorMessage,
+} from "../../redux/entities/dish/slice";
 import { DishPage } from "./dish-page";
+import { useRequest } from "../../redux/hooks/use-request";
+import { getDishById } from "../../redux/entities/dish/get-dish-by-id";
 
 export const DishPageContainer: FC = () => {
   const { dishId } = useParams();
 
+  // @ts-expect-error: Type 'unknown' is not assignable to type 'string'.
+  const requestStatus = useRequest(getDishById, dishId);
   const dish = useSelector<IRootState, ReturnType<typeof selectDishById>>(
     (state) => selectDishById(state, String(dishId))
   );
+  const errorMessage = useSelector(selectErrorMessage);
 
-  if (!dish) {
-    return null;
-  }
-
-  return <DishPage dish={dish} />;
+  return (
+    <DishPage
+      dish={dish}
+      requestStatus={requestStatus}
+      errorMessage={errorMessage}
+    />
+  );
 };
--- src/pages/DishPage/dish-page.tsx
+++ src/pages/DishPage/dish-page.tsx
@@ -1,34 +1,71 @@
 import { FC, use } from "react";
-import { IDishNormalized } from "../../types";
+import { IDishNormalized, RequestStatus } from "../../types";
 import { AuthContext } from "../../components/AuthContextProvider/auth-context";
 import { DishCounter } from "../../components/DishCounter/dish-counter";
+import { Skeleton } from "../../components/Skeleton/skeleton";
+import { Notification } from "../../components/Notification/notification";
 
 import styles from "./dish-page.module.css";
 
 interface IDishPageProps {
-  dish: IDishNormalized;
+  dish: IDishNormalized | undefined;
+  requestStatus: RequestStatus;
+  errorMessage: string;
 }
 
-export const DishPage: FC<IDishPageProps> = ({ dish }) => {
+export const DishPage: FC<IDishPageProps> = ({
+  dish,
+  requestStatus,
+  errorMessage,
+}) => {
   const { isAuthorized } = use(AuthContext);
-  const { id, name, ingredients, price } = dish;
+  const isPending = requestStatus === RequestStatus.pending;
+  const isRejected = requestStatus === RequestStatus.rejected;
 
   return (
     <div className={styles.root}>
-      <h1>{name}</h1>
+      {isPending ? (
+        <Skeleton
+          variant="text"
+          width={200}
+          fontSize={24}
+          className={styles.skeletonName}
+        />
+      ) : (
+        <h1>{dish?.name}</h1>
+      )}
       <div className={styles.block}>
         <h3>Ingredients</h3>
-        <ul role="list">
-          {ingredients.map((ingredient) => (
-            <li key={ingredient}>{ingredient}</li>
-          ))}
-        </ul>
+        {isPending ? (
+          <Skeleton variant="rectangular" width={200} height={50} />
+        ) : (
+          <ul role="list">
+            {(dish?.ingredients ?? []).map((ingredient) => (
+              <li key={ingredient}>{ingredient}</li>
+            ))}
+          </ul>
+        )}
       </div>
       <div className={styles.block}>
         <h3>Price</h3>
-        <div>{price} USD</div>
+        {isPending ? (
+          <Skeleton
+            variant="text"
+            width={200}
+            fontSize={16}
+            className={styles.skeletonPrice}
+          />
+        ) : (
+          <div>{dish?.price} USD</div>
+        )}
       </div>
-      {isAuthorized && <DishCounter id={id} />}
+      {isAuthorized &&
+        (isPending ? (
+          <Skeleton variant="rectangular" width={100} height={31} />
+        ) : (
+          !isRejected && dish && <DishCounter id={dish.id} />
+        ))}
+      {isRejected && <Notification message={errorMessage} />}
     </div>
   );
 };
