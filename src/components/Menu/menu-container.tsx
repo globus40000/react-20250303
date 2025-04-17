b6b78bb52602ba1746b3315b3287e1254f8079b2
Pavel Ashifin <globus40000@gmail.com>
2025-04-17 11:28:28 +0600

    feat: get dishes for restaurant from api

--- src/components/Menu/menu-container.tsx
+++ src/components/Menu/menu-container.tsx
@@ -1,25 +1,29 @@
 import { FC } from "react";
 import { Identifier } from "../../types";
 import { useSelector } from "react-redux";
-import { IRootState } from "../../redux/store";
-import { selectRestaurantById } from "../../redux/entities/restaurant/slice";
 import { Menu } from "./menu";
+import { useRequest } from "../../redux/hooks/use-request";
+import { getDishesForRestaurant } from "../../redux/entities/dish/get-dishes-for-restaurant";
+import {
+  selectDishesIds,
+  selectErrorMessage,
+} from "../../redux/entities/dish/slice";
 
 interface IMenuContainerProps {
   id: Identifier;
 }
 
 export const MenuContainer: FC<IMenuContainerProps> = ({ id }) => {
-  const restaurant = useSelector<
-    IRootState,
-    ReturnType<typeof selectRestaurantById>
-  >((state) => selectRestaurantById(state, id));
+  // @ts-expect-error: Type 'unknown' is not assignable to type 'string'.
+  const requestStatus = useRequest(getDishesForRestaurant, id);
+  const dishesIds = useSelector(selectDishesIds);
+  const errorMessage = useSelector(selectErrorMessage);
 
-  if (!restaurant) {
-    return null;
-  }
-
-  const { menu } = restaurant;
-
-  return <Menu dishesIds={menu} />;
+  return (
+    <Menu
+      dishesIds={dishesIds}
+      requestStatus={requestStatus}
+      errorMessage={errorMessage}
+    />
+  );
 };
--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -1,26 +1,41 @@
 import { FC } from "react";
-import { Identifier } from "../../types";
+import { Identifier, RequestStatus } from "../../types";
 import classNames from "classnames";
 import { DishLinkContainer } from "../DishLink/dish-link-container";
+import { Skeleton } from "../Skeleton/skeleton";
+import { Notification } from "../Notification/notification";
 
 import styles from "./menu.module.css";
 
 interface IMenuProps {
   dishesIds: Identifier[];
+  requestStatus: RequestStatus;
+  errorMessage: string;
   className?: string;
 }
 
-export const Menu: FC<IMenuProps> = ({ dishesIds, className }) => {
+export const Menu: FC<IMenuProps> = ({
+  dishesIds,
+  requestStatus,
+  errorMessage,
+  className,
+}) => {
   return (
     <div className={classNames(styles.root, className)}>
       <h3>Menu</h3>
-      <ul role="list">
-        {dishesIds.map((id) => (
-          <li key={id}>
-            <DishLinkContainer id={id} />
-          </li>
-        ))}
-      </ul>
+      {requestStatus === RequestStatus.pending ? (
+        <Skeleton variant="rectangular" width={200} height={62} />
+      ) : requestStatus === RequestStatus.rejected ? (
+        <Notification message={errorMessage} />
+      ) : (
+        <ul role="list">
+          {dishesIds.map((id) => (
+            <li key={id}>
+              <DishLinkContainer id={id} />
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
