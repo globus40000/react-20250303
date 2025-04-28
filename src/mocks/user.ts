a8585f2a3459112e2a81344eb2edc43c58cea4fb
Pavel Ashifin <globus40000@gmail.com>
2025-04-28 18:01:41 +0600

    refactor: use mock user from simple_api

--- src/mocks/user.ts
+++ src/mocks/user.ts
@@ -1,6 +1,6 @@
 import { IUserNormalized } from "../types";
 
 export const user: IUserNormalized = {
-  id: "abc",
-  name: "John",
+  id: "a304959a-76c0-4b34-954a-b38dbf310360",
+  name: "Antony",
 };
